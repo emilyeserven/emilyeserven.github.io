02dd3a1884dd91533f9523869d15baaf009d16c4
Emily Serven <emily@servenupwebsites.net>
2025-08-11 19:00:03 -0400

    Mapping header items like I should've, add shadui elements

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,6 @@
 export function Header() {
+    const menuItems = ["About", "Projects", "Blog", "Contact"];
+
     return (
         <div className="bg-white/70 backdrop-blur-sm border-b-2">
             <div className="container py-2 flex justify-between items-center">
@@ -6,11 +8,10 @@ export function Header() {
                     <p className="text-2xl font-bold">Emily Serven</p>
                     <p className="text-md font-bold">Software Engineer</p>
                 </div>
-                <ul className="text-lg font-medium flex justify-end list-none gap-8">
-                    <li>About</li>
-                    <li>Projects</li>
-                    <li>Blog</li>
-                    <li>Contact</li>
+                <ul className="text-md font-medium flex justify-end list-none gap-4">
+                    {menuItems.map((item) => (
+                        <li className="bg-transparent px-4 py-2 hover:bg-black hover:text-white hover:cursor-pointer">{item}</li>
+                    ))}
                 </ul>
             </div>
         </div>

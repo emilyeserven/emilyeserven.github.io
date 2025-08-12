2b94cdcb53f78a90c190d94232d476c6a2476d63
Emily Serven <emily@servenupwebsites.net>
2025-08-11 22:24:47 -0400

    Prettiered Button code

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,27 +1,50 @@
-import {Button as ShadButton} from "@/components/shad/ui/button.tsx";
-import {cn} from "@/lib/utils.ts";
-import {LucideMoveRight} from "lucide-react";
+import { Button as ShadButton } from "@/components/shad/ui/button.tsx";
+import { cn } from "@/lib/utils.ts";
+import { LucideMoveRight } from "lucide-react";
 
 interface ButtonProps {
-    variant?: "default" | "destructive" | "outline" | "secondary" | "ghost" | "link",
-    size?: "default" | "sm" | "lg" | "icon",
-    isArrowShown?: boolean,
-    children: React.ReactNode | string
-    classNames?: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link";
+  size?: "default" | "sm" | "lg" | "icon";
+  isArrowShown?: boolean;
+  children: React.ReactNode | string;
+  classNames?: string;
+  onClick?: () => void;
 }
 
-export function Button({variant = "default", size = "default", isArrowShown = false, classNames, children}: ButtonProps)  {
-
-    return (
-        <ShadButton variant={variant} size={size} className={`
+export function Button({
+  variant = "default",
+  size = "default",
+  isArrowShown = false,
+  classNames,
+  children,
+    onClick
+}: ButtonProps) {
+  return (
+    <ShadButton
+      variant={variant}
+      size={size}
+      className={`
         rounded-none border-2 hover:cursor-pointer
         ${cn({
-            "border-primary hover:bg-white hover:text-primary": variant === "default",
-            "border-black hover:bg-black hover:text-white": variant === "outline",
-            "bg-black text-white border-black hover:bg-white hover:text-black": variant === "secondary",
-            "text-lg": size === "lg"
+          "border-primary hover:bg-white hover:text-primary":
+            variant === "default",
+          "border-black hover:bg-black hover:text-white": variant === "outline",
+          "bg-black text-white border-black hover:bg-white hover:text-black":
+            variant === "secondary",
+          "text-lg": size === "lg",
         })}
         ${classNames}
-        `}>{children}{isArrowShown && <LucideMoveRight />}</ShadButton>
-    )
+        `}
+      onClick={onClick ?? undefined}
+    >
+      {children}
+      {isArrowShown && <LucideMoveRight />}
+    </ShadButton>
+  );
 }

a99aee2661f454150cab07db9f9f9e0e8f590743
Emily Serven <emily@servenupwebsites.net>
2025-08-11 22:24:16 -0400

    Added logo to header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import {NavigationMenu} from "@/components/ui/NavigationMenu.tsx";
+import {Logo} from "@/components/ui/Logo.tsx";
 
 export function Header() {
     const menuItems = ["About", "Projects", "Blog", "Contact"];
@@ -6,9 +7,12 @@ export function Header() {
     return (
         <div className="bg-white/70 backdrop-blur-sm border-b-2">
             <div className="container py-2 flex justify-between items-center">
-                <div>
+                <div className="flex gap-x-2 items-center">
+                    <Logo mode="icon" theme="black" />
+                    <div>
                     <p className="text-2xl font-bold">Emily Serven</p>
                     <p className="text-md font-bold">Software Engineer</p>
+                    </div>
                 </div>
                 <NavigationMenu menuItems={menuItems} />
             </div>
